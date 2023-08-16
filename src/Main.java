d4578db04e070ee47d09d99b7bbcf2772d3bddd0
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-16 13:36:52 +0500

    Mean arith

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Введи 10 чисел, а я найду их среднее арифметическое");
+            int number;
+            int cointNum = 1;
+            int sum = 0;
+            while (cointNum <= 10) {
+                number = scanner.nextInt();
+                sum += number;
+                cointNum++;
+            }
+            int meanArith = sum / 10;
+            System.out.println("Среднее арифметическое твоих чисел: " + meanArith);
+        }catch (Exception ex) {
+            System.out.println("Ты ввёл что то не так");
+        }
+        }
     }
-}
+
